d1e79bde9338a0913591e0236d56e39e29c816d4
Mike Lovelace <mike.lovelace@imaginecommunications.com>
2016-04-29 16:12:53 -0400

    Add tests (Starting to anyway)

---
+++ angular2-jwt.spec.ts
@@ -0,0 +1,45 @@
+import "core-js";
+import {AuthConfig} from "./angular2-jwt";
+
+
+describe('AuthConfig', ()=> {
+    'use strict';
+
+    it('should have default values', ()=> {
+        const config = new AuthConfig().getConfig();
+        expect(config).toBeDefined();
+        expect(config.headerName).toBe("Authorization");
+        expect(config.headerPrefix).toBe("Bearer ");
+        expect(config.tokenName).toBe("id_token");
+        expect(config.noJwtError).toBe(false);
+        expect(config.noTokenScheme).toBe(false);
+        expect(config.globalHeaders).toEqual([]);
+        expect(config.tokenGetter).toBeDefined();
+        const token = "Token";
+        localStorage.setItem(config.tokenName, token);
+        expect(config.tokenGetter()).toBe(token);
+    });
+
+    it('should have default values', ()=> {
+        const configExpected = {
+            headerName: "Foo",
+            headerPrefix: "Bar",
+            tokenName: "token",
+            tokenGetter: ()=>"this is a token",
+            noJwtError: true,
+            globalHeaders: [{"header": "value"}, {"header2": "value2"}],
+            noTokenScheme: true
+        };
+        const config = new AuthConfig(configExpected).getConfig();
+        expect(config).toBeDefined();
+        expect(config.headerName).toBe(configExpected.headerName);
+        expect(config.headerPrefix).toBe(configExpected.headerPrefix + " ");
+        expect(config.tokenName).toBe(configExpected.tokenName);
+        expect(config.noJwtError).toBe(configExpected.noJwtError);
+        expect(config.noTokenScheme).toBe(configExpected.noTokenScheme);
+        expect(config.globalHeaders).toEqual(configExpected.globalHeaders);
+        expect(config.tokenGetter).toBeDefined();
+        expect(config.tokenGetter()).toBe("this is a token");
+    });
+
+});
