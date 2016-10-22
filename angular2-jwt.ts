c224adaa69d9ec58a1d0ecfef7966eb16b460685
Ryan Chenkie <ryanchenkie@gmail.com>
2016-10-22 19:34:46 -0400

    format objectAssign

--- angular2-jwt.ts
+++ angular2-jwt.ts
@@ -281,36 +281,35 @@ let hasOwnProperty = Object.prototype.hasOwnProperty;
 let propIsEnumerable = Object.prototype.propertyIsEnumerable;
 
 function toObject(val: any) {
-	if (val === null || val === undefined) {
-		throw new TypeError('Object.assign cannot be called with null or undefined');
-	}
-
-	return Object(val);
+  if (val === null || val === undefined) {
+    throw new TypeError('Object.assign cannot be called with null or undefined');
+  }
+  
+  return Object(val);
 }
 
 function objectAssign(target: any, ...source: any[]) {
-	let from: any;
-	let to = toObject(target);
-	let symbols: any;
-
-	for (var s = 1; s < arguments.length; s++) {
-		from = Object(arguments[s]);
-
-		for (var key in from) {
-			if (hasOwnProperty.call(from, key)) {
-				to[key] = from[key];
-			}
-		}
-
-		if ((<any>Object).getOwnPropertySymbols) {
-			symbols = (<any>Object).getOwnPropertySymbols(from);
-			for (var i = 0; i < symbols.length; i++) {
-				if (propIsEnumerable.call(from, symbols[i])) {
-					to[symbols[i]] = from[symbols[i]];
-				}
-			}
-		}
-	}
-
-	return to;
+  let from: any;
+  let to = toObject(target);
+  let symbols: any;
+  
+  for (var s = 1; s < arguments.length; s++) {
+    from = Object(arguments[s]);
+    
+    for (var key in from) {
+      if (hasOwnProperty.call(from, key)) {
+        to[key] = from[key];
+      }
+    }
+    
+    if ((<any>Object).getOwnPropertySymbols) {
+      symbols = (<any>Object).getOwnPropertySymbols(from);
+      for (var i = 0; i < symbols.length; i++) {
+        if (propIsEnumerable.call(from, symbols[i])) {
+          to[symbols[i]] = from[symbols[i]];
+        }
+      }
+    }
+  }
+  return to;
 }
