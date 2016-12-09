0d049506f4cb09b1afba9c500634929e7c08037e
Oleksandr Yamkov <a.yamkov@qipu.de>
2016-12-09 11:15:18 +0100

    #207: Added a test to check default tokenGetter with custom token name

--- angular2-jwt.spec.ts
+++ angular2-jwt.spec.ts
@@ -45,6 +45,16 @@ describe('AuthConfig', ()=> {
         expect(config.tokenGetter).toBeDefined();
         expect(config.tokenGetter()).toBe("this is a token");
     });
+    
+    it('should use custom token name in default tokenGetter', ()=> {
+      const configExpected = { tokenName: 'Token' };
+      const token = 'token';
+      const config = new AuthConfig(configExpected).getConfig();
+      localStorage.setItem(configExpected.tokenName, token);
+      expect(config).toBeDefined();
+      expect(config.tokenName).toBe(configExpected.tokenName);
+      expect(config.tokenGetter()).toBe(token);
+    });
 
 });
 
